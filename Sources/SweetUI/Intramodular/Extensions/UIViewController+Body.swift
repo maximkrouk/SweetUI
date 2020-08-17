c2f921defa29aae6774c8ea5a940dbaffd80d81a
maximkrouk <id.maximkrouk@gmail.com>
2020-08-17 13:20:01 +0300

    setBody method improvements

--- Sources/SweetUI/Intramodular/Extensions/UIViewController+Body.swift
+++ Sources/SweetUI/Intramodular/Extensions/UIViewController+Body.swift
@@ -8,24 +8,44 @@
 import UICocoa
 
 extension UIViewController {
-    public func setBody(content: () -> UIView) {
+    /// Sets a new view to a `view` property
+    ///
+    /// Note:
+    /// - Layout config for Layout and View proxies is dropped
+    public func setBody(content: () -> UIViewProvider) {
         setBody(content())
     }
     
-    public func setBody(_ content: UIView) {
-        self.view = content
+    /// Sets a new view to a `view` property
+    ///
+    /// Note:
+    /// - Layout config for Layout and View proxies is dropped
+    public func setBody(_ content: UIViewProvider) {
+        self.view = content.view
     }
 }
 
 extension UIView {
-    public func setBody(content: () -> UIView) {
+    /// Sets a new body to a view
+    ///
+    /// Note:
+    /// - This method removes all the subviews
+    /// - This method constrain content edges to self
+    /// - Layout config for Layout and View proxies is dropped
+    public func setBody(content: () -> UIViewProvider) {
         setBody(content())
     }
     
-    public func setBody(_ content: UIView) {
+    /// Sets a new body to a view
+    ///
+    /// Note:
+    /// - This method removes all the subviews
+    /// - This method constrain content edges to self
+    /// - Layout config for Layout and View proxies is dropped
+    public func setBody(_ content: UIViewProvider) {
         discard(subviews.map { $0.removeFromSuperview() })
-        addSubview(content)
-        content.layout.edges
+        addSubview(content.view)
+        content.view.layout.edges
             .equalToSuperview()
             .perform()
     }
